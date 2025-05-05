88a3bfae418bd04b5601c7d2ea07c4bf0dc7fca6
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-05 09:41:19 +0200

    Ajout du constructeur LieuRecharge + getters et setters

--- src/com/humanbooster/model/LieuRecharge.java
+++ src/com/humanbooster/model/LieuRecharge.java
@@ -1,4 +1,40 @@
 package src.com.humanbooster.model;
 
+import java.util.List;
+
 public class LieuRecharge {
+
+    private int id;
+    private String nom;
+    private List<BorneRecharge> bornes;
+
+    public LieuRecharge(int id, String nom, List<BorneRecharge> bornes) {
+        this.id = id;
+        this.nom = nom;
+        this.bornes = bornes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<BorneRecharge> getBornes() {
+        return bornes;
+    }
+
+    public void setBornes(List<BorneRecharge> bornes) {
+        this.bornes = bornes;
+    }
 }
