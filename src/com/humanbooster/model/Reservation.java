ba8e6cd3e13f63e0f3d23b3715e620247429dad7
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-05 09:47:17 +0200

    Ajout du Reservation + getters et setters

--- src/com/humanbooster/model/Reservation.java
+++ src/com/humanbooster/model/Reservation.java
@@ -1,4 +1,70 @@
 package src.com.humanbooster.model;
 
+import java.time.LocalDateTime;
+
 public class Reservation {
+
+    private int id;
+    private Utilisateur utilisateur;
+    private BorneRecharge borne;
+    private LocalDateTime dateDebut;
+    private LocalDateTime dateFin;
+    private StatutReservation statut;
+
+    public Reservation(int id, Utilisateur utilisateur, BorneRecharge borne, LocalDateTime dateDebut, LocalDateTime dateFin, StatutReservation statut) {
+        this.id = id;
+        this.utilisateur = utilisateur;
+        this.borne = borne;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public BorneRecharge getBorne() {
+        return borne;
+    }
+
+    public void setBorne(BorneRecharge borne) {
+        this.borne = borne;
+    }
+
+    public LocalDateTime getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDateTime dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDateTime getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDateTime dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public StatutReservation getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutReservation statut) {
+        this.statut = statut;
+    }
 }
