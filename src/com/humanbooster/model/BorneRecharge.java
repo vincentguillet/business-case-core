e2399a64273a52ace62583a346256bcb11fbe174
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-05 09:44:23 +0200

    Ajout du constructeur BorneRecharge + getters et setters

--- src/com/humanbooster/model/BorneRecharge.java
+++ src/com/humanbooster/model/BorneRecharge.java
@@ -1,4 +1,38 @@
 package src.com.humanbooster.model;
 
 public class BorneRecharge {
+
+    private int id;
+    private EtatBorne etat;
+    private int tarifHoraire; // Exprimé en centimes
+
+    public BorneRecharge(int id, EtatBorne etat, int tarifHoraire) {
+        this.id = id;
+        this.etat = etat;
+        this.tarifHoraire = tarifHoraire;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public EtatBorne getEtat() {
+        return etat;
+    }
+
+    public void setEtat(EtatBorne etat) {
+        this.etat = etat;
+    }
+
+    public int getTarifHoraire() {
+        return tarifHoraire;
+    }
+
+    public void setTarifHoraire(int tarifHoraire) {
+        this.tarifHoraire = tarifHoraire;
+    }
 }
