f8713d452fca7ac44a1d92502bf937fb35d780aa
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-21 09:41:55 +0200

    Ajout de la configuration Hibernate pour la connexion à la base de données

---
+++ src/com/humanbooster/config/HibernateConfig.java
@@ -0,0 +1,42 @@
+package src.com.humanbooster.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConfig {
+
+    private static final SessionFactory sessionFactory;
+    public static final boolean LOCAL = true;
+    private static String url;
+    private static String username;
+    private static String password;
+
+
+    static {
+
+        if (LOCAL) {
+            url = "jdbc:mysql://127.0.0.1:3306/electricity-business";
+            username = "admin";
+            password = "admin";
+        } else {
+            url = "jdbc:mysql://mysql:3306/electricity-business?useSSL=false&amp;allowPublicKeyRetrieval=true";
+            username = "root";
+            password = "root";
+        }
+
+        Configuration config = new Configuration()
+                .setProperty("hibernate.connection.url", url)
+                .setProperty("hibernate.connection.username", username)
+                .setProperty("hibernate.connection.password", password)
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.hbm2ddl.auto", "update")
+                .setProperty("hibernate.show_sql", "false")
+                .setProperty("hibernate.format_sql", "true");
+
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
